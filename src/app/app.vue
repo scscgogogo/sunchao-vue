24a4fb646d45c345ea070bd1a8a847ce42fe727b
scscgogogo <20456992@qq.com>
2022-04-20 07:25:00 +0800

    复选框

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,5 +1,24 @@
 <template>
   <h3>{{ name }}</h3>
+  <label for="tag-1">
+    <input
+      type="checkbox"
+      id="tag-1"
+      v-model="tags"
+      value="千岛湖"
+      @change="setName"
+    />
+    千岛湖
+  </label>
+  <label for="tag-2">
+    <input type="checkbox" id="tag-2" v-model="tags" value="太湖" />
+    太湖
+  </label>
+  <label for="tag-3">
+    <input type="checkbox" id="tag-3" v-model="tags" value="云龙湖" />
+    云龙湖
+  </label>
+  <span>→Tags:{{ tags }}</span>
 </template>
 
 <script>
@@ -7,6 +26,7 @@ export default {
   data() {
     return {
       name: 'SUNCHAO',
+      tags: [],
     };
   },
 };
